abab0cfac1348617ca4398bc0a6fcfb1d29bab27
SionSmallman <32775963+SionSmallman@users.noreply.github.com>
2024-09-09 20:28:10 +0100

    Updated location

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className='flex flex-col justify-center items-center w-screen h-screen bg-gradient-to-b from-[#300171] to-gray-900'>
         <h1>Siôn Smallman</h1>
-        <h2 className="">Cardiff, UK</h2>
+        <h2 className="">South Wales, UK</h2>
         <HomeButtonDrawer />
         <div className="absolute bottom-0 py-5">
             <h2>An actual website is coming soon...</h2>
